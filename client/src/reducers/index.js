cca7c387e71d53062a341ddbd92dc4042183e9b6
Gopal roy <roy.gopal1992@gmail.com>
2020-06-11 15:12:28 +0530

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from './BookReducer';
 
+/*
+ Combine reducers takes a hash of reducers and returns a reducer. 
+ The resulting reducer represents an object of the same shape as the hash.
+*/
 export default combineReducers({
     books: bookReducer
 });
